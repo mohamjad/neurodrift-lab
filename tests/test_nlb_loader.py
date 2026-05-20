b42e222bb8e5f1a7756414edb9cf86ecf519f69f
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:28:15 -0400

    test: add test_nlb_h5_loader_builds_even_odd_pair

--- tests/test_nlb_loader.py
+++ tests/test_nlb_loader.py
@@ -3,3 +3,8 @@ from __future__ import annotations
 import numpy as np
 
 from neurodrift.datasets.nlb import load_nlb_h5_pair
+
+
+def test_nlb_h5_loader_builds_even_odd_pair(tmp_path) -> None:
+    import h5py
+
