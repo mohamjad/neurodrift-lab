7827650dbce9957b459d681414875549933ac842
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:27:50 -0400

    test: add test_simulated_pair_has_expected_shapes

--- tests/test_simulation_and_env.py
+++ tests/test_simulation_and_env.py
@@ -3,3 +3,8 @@ from __future__ import annotations
 from neurodrift.envs.intent_drift import IntentDriftEnv
 from neurodrift.models.alignment import ProcrustesAligner, WhiteningColoringAligner
 from neurodrift.simulation import SimulationConfig, simulate_session_pair
+
+
+def test_simulated_pair_has_expected_shapes() -> None:
+    config = SimulationConfig(trials=32, time_steps=12, channels=7, intent_dims=2, latent_dims=4)
+    pair = simulate_session_pair(config)
