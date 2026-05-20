37712419d5437c157e81a9e171b5334390a6092a
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:22:46 -0400

    core: wire --init-- imports

---
+++ src/neurodrift/__init__.py
@@ -0,0 +1,5 @@
+"""NeuroDrift Lab public package interface."""
+
+from neurodrift.benchmarks import AlignmentBenchmark, run_alignment_benchmark
+from neurodrift.session import SessionBatch, SessionPair
+
