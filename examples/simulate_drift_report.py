de901d5d10bc395aca869657263314f6d79b37ea
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:27:26 -0400

    example: wire simulate-drift-report imports

--- examples/simulate_drift_report.py
+++ examples/simulate_drift_report.py
@@ -3,3 +3,8 @@
 from __future__ import annotations
 
 import json
+
+from neurodrift.envs.intent_drift import IntentDriftEnv
+from neurodrift.models.alignment import ProcrustesAligner, WhiteningColoringAligner
+from neurodrift.simulation import SimulationConfig, simulate_session_pair
+
