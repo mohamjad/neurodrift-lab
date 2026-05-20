7dd984bb1cfdd3745d187707b38a1c87b9759d59
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:27:36 -0400

    test: add test_affine_invariant_distance_is_zero_for_same_matrix

--- tests/test_riemannian_metrics.py
+++ tests/test_riemannian_metrics.py
@@ -3,3 +3,8 @@ from __future__ import annotations
 import numpy as np
 
 from neurodrift.metrics.riemannian import affine_invariant_distance, covariance_drift
+from neurodrift.numerics import safe_covariance
+
+
+def test_affine_invariant_distance_is_zero_for_same_matrix() -> None:
+    samples = np.random.default_rng(1).normal(size=(64, 5))
