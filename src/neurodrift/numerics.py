cfaf7fa452c180172c1d93f2ae82d0530fea40de
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:22:51 -0400

    math: add as_float_array

--- src/neurodrift/numerics.py
+++ src/neurodrift/numerics.py
@@ -8,3 +8,8 @@ from numpy.typing import NDArray
 Array = NDArray[np.float64]
 
 
+def as_float_array(value: Array) -> Array:
+    """Return a contiguous float64 array."""
+
+    return np.asarray(value, dtype=np.float64)
+
