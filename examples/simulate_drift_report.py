6ea4ee03d3a52ff4801e18bc43c3e65d293c6bc3
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:27:27 -0400

    example: add main

--- examples/simulate_drift_report.py
+++ examples/simulate_drift_report.py
@@ -8,3 +8,8 @@ from neurodrift.envs.intent_drift import IntentDriftEnv
 from neurodrift.models.alignment import ProcrustesAligner, WhiteningColoringAligner
 from neurodrift.simulation import SimulationConfig, simulate_session_pair
 
+
+def main() -> None:
+    pair = simulate_session_pair(SimulationConfig(seed=11, drift_strength=0.22))
+    env = IntentDriftEnv(pair)
+    baseline = env.evaluate(ProcrustesAligner())
