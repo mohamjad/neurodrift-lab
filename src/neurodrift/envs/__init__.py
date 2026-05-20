7410bf00627e8321f057083c3c5bae87239a9cd6
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:25:13 -0400

    env: wire --init-- imports

---
+++ src/neurodrift/envs/__init__.py
@@ -0,0 +1,5 @@
+"""Evaluation environments for BCI drift experiments."""
+
+from neurodrift.envs.intent_drift import DriftEvaluationResult, IntentDriftEnv
+
+__all__ = ["DriftEvaluationResult", "IntentDriftEnv"]
