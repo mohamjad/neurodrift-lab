8856b138918a6af84cda10c42feefa58a0318e2e
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:28:17 -0400

    test: refine test-nlb-loader

--- tests/test_nlb_loader.py
+++ tests/test_nlb_loader.py
@@ -13,3 +13,8 @@ def test_nlb_h5_loader_builds_even_odd_pair(tmp_path) -> None:
         group = handle.create_group("mc_maze_small_20")
         group.create_dataset("eval_spikes_heldout", data=np.ones((12, 5, 4)))
         group.create_dataset("eval_behavior", data=np.ones((12, 5, 2)))
+
+    pair = load_nlb_h5_pair(path, max_trials=10)
+
+    assert pair.source.neural.shape == (5, 5, 4)
+    assert pair.target.neural.shape == (5, 5, 4)
