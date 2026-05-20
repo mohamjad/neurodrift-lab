2403378739ea1e60a25538f7b7b3cbb89cdc2625
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:24:32 -0400

    models: wire alignment imports

--- src/neurodrift/models/alignment.py
+++ src/neurodrift/models/alignment.py
@@ -8,3 +8,8 @@ from typing import Protocol
 import numpy as np
 from numpy.typing import NDArray
 
+from neurodrift.metrics.subspace import orthogonal_procrustes
+from neurodrift.numerics import matrix_invsqrt_spd, matrix_sqrt_spd, safe_covariance
+
+Array = NDArray[np.float64]
+
