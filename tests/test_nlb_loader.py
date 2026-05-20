1e6bdefa99f957f68e582e0adf089a8388ed3857
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:28:16 -0400

    test: refine test-nlb-loader

--- tests/test_nlb_loader.py
+++ tests/test_nlb_loader.py
@@ -8,3 +8,8 @@ from neurodrift.datasets.nlb import load_nlb_h5_pair
 def test_nlb_h5_loader_builds_even_odd_pair(tmp_path) -> None:
     import h5py
 
+    path = tmp_path / "nlb.h5"
+    with h5py.File(path, "w") as handle:
+        group = handle.create_group("mc_maze_small_20")
+        group.create_dataset("eval_spikes_heldout", data=np.ones((12, 5, 4)))
+        group.create_dataset("eval_behavior", data=np.ones((12, 5, 2)))
