11f07880bff122f5384d6a355cbf7070da6e0b62
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:22:47 -0400

    core: refine --init--

--- src/neurodrift/__init__.py
+++ src/neurodrift/__init__.py
@@ -3,3 +3,8 @@
 from neurodrift.benchmarks import AlignmentBenchmark, run_alignment_benchmark
 from neurodrift.session import SessionBatch, SessionPair
 
+__all__ = [
+    "AlignmentBenchmark",
+    "SessionBatch",
+    "SessionPair",
+    "run_alignment_benchmark",
