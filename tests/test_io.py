00d00a222d0660a36542853cd6ee7c77063a98c3
mohamjad <mohammedamjad526@gmail.com>
2026-05-20 15:28:02 -0400

    test: wire test-io imports

--- tests/test_io.py
+++ tests/test_io.py
@@ -3,3 +3,8 @@ from __future__ import annotations
 import json
 
 import numpy as np
+
+from neurodrift.io import load_session_pair_npz, save_json, save_session_pair_npz
+from neurodrift.simulation import SimulationConfig, simulate_session_pair
+
+
