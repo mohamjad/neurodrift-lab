24540dee9e2045c831beb875918ac5195b34095c
mohamjad <mohammedamjad526@gmail.com>
2026-05-21 20:20:41 -0400

    pkg: expose intent plasticity api

--- src/neurodrift/__init__.py
+++ src/neurodrift/__init__.py
@@ -1,11 +1,17 @@
 """NeuroDrift Lab public package interface."""
 
 from neurodrift.benchmarks import AlignmentBenchmark, run_alignment_benchmark
+from neurodrift.intent import IntentDistribution, soft_intent_from_observations
+from neurodrift.plasticity import PlasticitySignal, build_plasticity_signal
 from neurodrift.session import SessionBatch, SessionPair
 
 __all__ = [
     "AlignmentBenchmark",
+    "IntentDistribution",
+    "PlasticitySignal",
     "SessionBatch",
     "SessionPair",
+    "build_plasticity_signal",
     "run_alignment_benchmark",
+    "soft_intent_from_observations",
 ]
